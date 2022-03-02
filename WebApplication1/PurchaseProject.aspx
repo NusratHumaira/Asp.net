e03700c2bc7cb37c1c997c0d998b9e4c7b0d96fa
nusrat_humaira <nusrat.humaira12@gmail.com>
2022-03-02 17:29:38 +0600

    purchase project added

---
+++ WebApplication1/PurchaseProject.aspx
@@ -0,0 +1,88 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="PurchaseProject.aspx.cs" Inherits="WebApplication1.PurchaseProject" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+     <form id="form1" runat="server">
+     <asp:HiddenField ID="hfPurchaseInfoID" runat="server" />
+        
+        <div>
+            <asp:TextBox ID="txtSearch" runat="server" Style="margin-top: 13px" Width="200px"></asp:TextBox>
+         
+            <asp:Button ID="btnSearch" runat="server" Text="Search" OnClick="Search" />
+        </div>
+         <div>
+         <h3><b>Purchase Information:</b></h3>
+         <b><asp:Label ID="purchaseIDLabel" runat="server" Text="Purchase Id: " Width="200px"></asp:Label></b>
+            <asp:TextBox ID="txtPurchaseID" runat="server" Width="160px" AutoPostBack="false"></asp:TextBox>
+            &nbsp;&nbsp;&nbsp;
+            <b><asp:Label ID="companyLabel" runat="server" Text="Company Name " Width="200px"></asp:Label></b>
+            <asp:DropDownList ID="companyDropDownList"  runat="server" Width="160px">
+                <asp:ListItem Value="0" text="Select a Value"></asp:ListItem>
+                <asp:ListItem>Runner Cyberlink Limited</asp:ListItem>
+                <asp:ListItem>Birds A & Z Limited</asp:ListItem>
+                <asp:ListItem>Birds Garments Limited</asp:ListItem>
+                <asp:ListItem>Birds Garments Limited(Unit 2)</asp:ListItem>
+                <asp:ListItem>Birds Apparels Limited</asp:ListItem>
+                <asp:ListItem>Birds Fadex Limited</asp:ListItem>
+                <asp:ListItem>Birds Air Transport Limited</asp:ListItem>
+                <asp:ListItem>Birds RNR Fashions Limited</asp:ListItem>
+            </asp:DropDownList>
+            <br />
+             <b><asp:Label ID="purchaseByLabel" runat="server" Text="Purchase By: " Width="200px"></asp:Label></b>
+            <asp:TextBox ID="txtPurchaseBy" runat="server" Width="160px" AutoPostBack="false"></asp:TextBox>
+            <br />
+             <b><asp:Label ID="dateLabel" runat="server" Text="Date: " Width="200px"></asp:Label></b>
+            <asp:TextBox ID="txtDate" runat="server" Width="160px" AutoPostBack="false"></asp:TextBox>
+          </div>
+          
+           <asp:HiddenField ID="hfPurchaseDetailsID" runat="server" />
+          <div>
+         <h3><b>Purchase Details Information:</b></h3>
+         <b><asp:Label ID="itemNameLabel" runat="server" Text="Item Name: " Width="200px"></asp:Label></b>
+            <asp:TextBox ID="txtItemName" runat="server" Width="160px" AutoPostBack="false"></asp:TextBox>
+            <br />
+             <b><asp:Label ID="quantityLabel" runat="server" Text="Quantity: " Width="200px"></asp:Label></b>
+            <asp:TextBox ID="txtQuantity" runat="server" Width="160px" AutoPostBack="false"></asp:TextBox>
+            <br />
+            <b><asp:Label ID="unitPriceLabel" runat="server" Text="Unit Price: " Width="200px"></asp:Label></b>
+            <asp:TextBox ID="txtUnitPrice" runat="server" Width="160px" AutoPostBack="false"></asp:TextBox>
+            <br />
+            <b><asp:Label ID="totalAmountLabel" runat="server" Text="Total Amount: " Width="200px"></asp:Label></b>
+            <asp:TextBox ID="txtTotalAmount" runat="server" Width="160px" AutoPostBack="false"></asp:TextBox>
+            <br />
+            <asp:Button ID="btnList" runat="server" Text="Add to the List" OnClick="btnAddToList_Click" />
+         </div>
+         <br />
+        <asp:Label ID="lblSuccessMessage" runat="server" Text="" ForeColor="Green" Width="200px"></asp:Label>
+        <br />
+        <asp:Label ID="lblErrorMessage" runat="server" Text="" ForeColor="Red" Width="200px"></asp:Label>
+        <br />
+        <asp:Button ID="btnSave" runat="server" Text="Save" OnClick="btnSave_Click" />
+        <asp:Button ID="btnClear" runat="server" Text="Refresh" OnClick="btnClear_Click" />
+    <asp:GridView ID="gvPurchase" runat="server" AutoGenerateColumns="false" OnRowEditing="gvPurchase_RowEditing">
+        <Columns>
+            <asp:BoundField DataField="purchaseID" HeaderText="Purchase ID" />
+            <asp:BoundField DataField="itemName" HeaderText="Item Name" />
+            <asp:BoundField DataField="quantity" HeaderText="Quantity" />
+            <asp:BoundField DataField="unitPrice" HeaderText="Unit Price" />
+            <asp:BoundField DataField="totalAmount" HeaderText="Total Amount" />
+            <asp:TemplateField>
+                <ItemTemplate>
+                    <asp:LinkButton ID="editGrid" Text="Edit" runat="server" CommandName="Edit" />
+                    <asp:LinkButton ID="deleteGrid" Text="Delete" runat="server" OnClick="OnDelete" />
+                </ItemTemplate>
+                <EditItemTemplate>
+                    <asp:LinkButton ID="updateGrid" Text="Update" runat="server" OnClick="OnUpdate" />
+                    <asp:LinkButton ID="cancelGrid" Text="Cancel" runat="server" OnClick="OnCancel" />
+                </EditItemTemplate>
+            </asp:TemplateField>
+        </Columns>
+    </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/PurchaseProject.aspx.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace WebApplication1
+{
+    public partial class PurchaseProject : System.Web.UI.Page
+    {
+        SqlConnection sqlCon = new SqlConnection("Data Source=NUSRAT\\SQL2012; database=AspDotnetVenkatDB;User ID=sa;Password=sa1234");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (!this.IsPostBack)
+            {
+                DataTable dt = new DataTable();
+                dt.Columns.Add("purchaseID");
+                dt.Columns.Add("itemName");
+                dt.Columns.Add("quantity");
+                dt.Columns.Add("unitPrice");
+                dt.Columns.Add("totalAmount");
+                ViewState["dt"] = dt;
+
+            }
+        }
+        protected void Search(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+        public void Clear()
+        {
+            hfPurchaseInfoID.Value = "";
+            txtPurchaseID.Text = txtPurchaseBy.Text = txtPurchaseBy.Text = txtDate.Text = String.Empty;
+            txtQuantity.Text = txtUnitPrice.Text = txtTotalAmount.Text = String.Empty;
+
+
+        }
+        protected void gvPurchase_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            gvPurchase.EditIndex = e.NewEditIndex;
+            gvPurchase.DataSource = ViewState["dt"] as DataTable;
+            gvPurchase.DataBind();
+        }
+        protected void btnAddToList_Click(object sender, EventArgs e)
+        {
+
+            DataTable dt = ViewState["dt"] as DataTable;
+            decimal totalPrice = Convert.ToDecimal(txtQuantity.Text) * Convert.ToDecimal(txtUnitPrice.Text);
+            dt.Rows.Add(txtPurchaseID.Text, txtItemName.Text, txtQuantity.Text, txtUnitPrice.Text, totalPrice);
+            txtTotalAmount.Text = totalPrice.ToString();
+            ViewState["dt"] = dt;
+
+            gvPurchase.DataSource = ViewState["dt"] as DataTable;
+            gvPurchase.DataBind();
+            txtItemName.Text = txtQuantity.Text = txtUnitPrice.Text = txtTotalAmount.Text = String.Empty;
+
+        }
+        protected void OnUpdate(object sender, EventArgs e)
+        {
+            GridViewRow row = (sender as LinkButton).NamingContainer as GridViewRow;
+            string itemname = ((TextBox)row.Cells[1].Controls[0]).Text;
+            decimal quantity = Convert.ToDecimal(((TextBox)row.Cells[2].Controls[0]).Text);
+            decimal unitPrice = Convert.ToDecimal(((TextBox)row.Cells[2].Controls[0]).Text);
+            decimal totalPrice = quantity * unitPrice;
+
+            DataTable dt = ViewState["dt"] as DataTable;
+            dt.Rows[row.RowIndex]["itemName"] = itemname;
+            dt.Rows[row.RowIndex]["quantity"] = quantity;
+            dt.Rows[row.RowIndex]["unitPrice"] = unitPrice;
+            dt.Rows[row.RowIndex]["totalAmount"] = totalPrice;
+            ViewState["dt"] = dt;
+            gvPurchase.EditIndex = -1;
+            gvPurchase.DataSource = ViewState["dt"] as DataTable;
+            gvPurchase.DataBind();
+        }
+        protected void OnCancel(object sender, EventArgs e)
+        {
+            gvPurchase.EditIndex = -1;
+            gvPurchase.DataSource = ViewState["dt"] as DataTable;
+            gvPurchase.DataBind();
+        }
+        protected void OnDelete(object sender, EventArgs e)
+        {
+            GridViewRow row = (sender as LinkButton).NamingContainer as GridViewRow;
+            DataTable dt = ViewState["dt"] as DataTable;
+            dt.Rows.RemoveAt(row.RowIndex);
+            ViewState["dt"] = dt;
+            gvPurchase.DataSource = ViewState["dt"] as DataTable;
+            gvPurchase.DataBind();
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            if (sqlCon.State == ConnectionState.Closed)
+                sqlCon.Open();
+            SqlCommand sqlCmd = new SqlCommand("InsertPurchaseInfo", sqlCon);
+            sqlCmd.CommandType = CommandType.StoredProcedure;
+            sqlCmd.Parameters.AddWithValue("@purchaseInfoID", (hfPurchaseInfoID.Value == "" ? 0 : Convert.ToInt32(hfPurchaseInfoID.Value)));
+
+            sqlCmd.Parameters.AddWithValue("@purchaseID", txtPurchaseID.Text.Trim());
+
+            sqlCmd.Parameters.AddWithValue("@company", companyDropDownList.SelectedValue);
+            sqlCmd.Parameters.AddWithValue("@purchaseBy", txtPurchaseBy.Text.Trim());
+            sqlCmd.Parameters.AddWithValue("@date", txtDate.Text.Trim());
+            sqlCmd.ExecuteNonQuery();
+            for (int i = 0; i < gvPurchase.Rows.Count; i++)
+            {
+                SqlCommand sqlCd = new SqlCommand("InsertPurchaseDetailsInfo", sqlCon);
+                sqlCd.CommandType = CommandType.StoredProcedure;
+                sqlCd.Parameters.AddWithValue("@purchaseID", txtPurchaseID.Text.Trim());
+                sqlCd.Parameters.AddWithValue("@itemName", gvPurchase.Rows[i].Cells[1].Text);
+                sqlCd.Parameters.AddWithValue("@quantity", gvPurchase.Rows[i].Cells[2].Text);
+                sqlCd.Parameters.AddWithValue("@unitPrice", gvPurchase.Rows[i].Cells[3].Text);
+                sqlCd.Parameters.AddWithValue("@totalAmount", gvPurchase.Rows[i].Cells[4].Text);
+                sqlCd.ExecuteNonQuery();
+            }
+
+
+            sqlCon.Close();
+            string ID = hfPurchaseInfoID.Value;
+            Clear();
+            if (ID == "")
+                lblSuccessMessage.Text = "Saved Successfully";
+            else
+                lblSuccessMessage.Text = "Updated Successfully";
+        }
+    }
+}
